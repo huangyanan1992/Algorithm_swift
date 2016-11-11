731d5fb897d336f1ce04a6b73704605a42cd1d16
SQHyn1992 <huangyanan@01zhuanche.com>
2016-11-11 15:01:18 +0800

    Initial Commit

---
+++ Algorithm_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithm_swift
+//
+//  Created by Huang Yanan on 2016/11/11.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
